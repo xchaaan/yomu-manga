31bed2109c33c5612878bee96f49d45f637017bd
Christian Sarmiento <christian.sarmiento.work@gmail.com>
2023-10-12 12:24:30 +0800

    updated key for redis to accomodate dynamic request

--- backend/app/manga/manga.py
+++ backend/app/manga/manga.py
@@ -4,7 +4,9 @@ from backend.app.manga.lib.utils import utils
 from backend.database.redis import r
 from backend.app.global_var import manga_dex_url
 from flask import current_app
+from flask import request
 
+import pymongo
 import requests
 import json
 
@@ -13,11 +15,19 @@ class Manga(Resource):
     Class for giving response containing all the necessary details for the specific manga
     Title, Chapter list and Author
     """
+    LIMIT = 100
 
     def get(self, title_key: str):
 
         if not title_key:
             return {'msg': 'title not found'}, 404
+        
+        # accept optional body with keys order and offset
+        # offset is use to get the subset of records starting with the given value
+        args = request.args
+
+        order = args.get('order', 'asc')
+        offset = int(args.get('offset', 0))
 
         # get the uuid of this title_key from the database
         document = manga_collection.find_one({'title_key': title_key})
@@ -36,48 +46,62 @@ class Manga(Resource):
 
         document = manga_collection.find_one({'title_key': title_key})
         manga_id = document['details']['id']
+        redis_key = f"{manga_id}-{order}-{str(offset)}" 
 
         # look first in the redis if we have cached the response
-        response = r.get(manga_id)
+        response = r.get(redis_key)
 
         if response:
             return json.loads(response), 200
 
         # we can look into the database. this way we can avoid sending request
+        order_type = pymongo.ASCENDING if order == 'asc' else pymongo.DESCENDING
         chapter_docs = chapter_collection.find({
             '$and': [
                 {'details.relationships.type': 'manga',},
                 {'details.relationships.id': manga_id,},
             ]
-        })
+        })\
+        .limit(self.LIMIT)\
+        .sort('details.attributes.chapters', order_type)\
+        .skip(offset)
+
+        params = {
+            'order': order,
+            'offset': offset,
+        }
 
         if not list(chapter_docs.clone()):
-            response = self._fetch(manga_id)
-
+            response = self._fetch(manga_id, **params)
+ 
             if not response:
                 return {'msg': f'no available chapter found for {title_key}'}, 404
 
-            _ = self._insert_record(response.get('data'))      
+            _ = self._insert_record(response.get('data'), title_key)      
         
-            wrapped_response = self._wrapped_response(response.get('data'))
+            wrapped_response = self._wrapped_response(response.get('data'), order)
         
         else:
-            wrapped_response = self._wrapped_response(chapter_docs)
-       
-        r.set(manga_id, json.dumps(wrapped_response))
+            print ("here")
+            wrapped_response = self._wrapped_response(chapter_docs, order)
+
+        r.set(redis_key, json.dumps(wrapped_response))
         return wrapped_response, 200
     
-    def _fetch(self, manga_id: str):
+    def _fetch(self, manga_id: str, **kwargs):
         # send a request to the API
         response = requests.get(
             f"{manga_dex_url}/chapter",
             params={
                 'manga': manga_id,
-                'limit': 100,
-                'order[chapter]': 'asc',
+                'limit': self.LIMIT,
+                'offset': kwargs.get('offset'),
+                'order[chapter]': kwargs.get('order'),
                 'translatedLanguage[]': 'en',
             }
         )
+
+        print (response.json())
 
         if not response:
             return {}
@@ -95,7 +119,7 @@ class Manga(Resource):
         return response
     
     @staticmethod
-    def _wrapped_response(data: dict):
+    def _wrapped_response(data: dict, order_type: str):
         if not data:
             return {}
         
@@ -107,11 +131,13 @@ class Manga(Resource):
             else:
                 wrapped_response[int(chapter['attributes']['chapter'])] = chapter
 
-        # sort the dictionary to asc order    
-        return dict(sorted(wrapped_response.items()))
+        # sort the dictionary to asc order  
+        return dict(sorted(
+            wrapped_response.items(), reverse=order_type=='desc'
+        ))
 
     @staticmethod 
-    def _insert_record(data: dict):
+    def _insert_record(data: dict, label: str):
         if not data:
             return False
 
@@ -120,7 +146,7 @@ class Manga(Resource):
             if chapter_collection.find_one({'id': chapter['id']}):
                 continue
             
-            chapter_collection.insert_one({'details': chapter})
+            chapter_collection.insert_one({'details': chapter, 'label': label})
 
         return True            
 
